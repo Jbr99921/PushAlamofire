9c928288bc0f72119031b33bd455b60a54a078da
Krishna7891 <krishna.prajapati@xongolab.com>
2018-10-10 10:06:33 +0530

    Initial Commit

---
+++ Alamofire123/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alamofire123
+//
+//  Created by XongoLab on 10/10/18.
+//  Copyright © 2018 XongoLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
